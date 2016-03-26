dcc27c2a52025edd7c01faeac114eeda5545bdb8
Konstantin Evchenko <evchenk2@illinois.edu>
2016-03-25 23:45:21 -0500

    Implemented a client that will send test cases to a server

--- replay-cli.cpp
+++ replay-cli.cpp
@@ -1,25 +1,94 @@
+#include "replay.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <fstream>
-#include <string>
-
-#include "replay.h"
+#include <unistd.h>
+#include <vector>
 
 // send `libpath` to worker waiting on `sockpath` and get response
-response *runOneTest(std::string sockpath, std::string libpath)
+response runOneTest(std::string sockpath, std::string libpath)
 {
+  response result;
+  sockaddr_un addr;
+
+  auto sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sock < 0) {
+    throw std::logic_error("Cannot create socket");
+  }
+
+  addr.sun_family = AF_UNIX;
+  sockpath.copy(addr.sun_path, sockpath.size());
+  addr.sun_path[sockpath.size()] = '\0';
+
+
+  if (connect(sock, (sockaddr *)(&addr), sizeof(addr)) < 0) {
+    close(sock);
+    throw std::logic_error("Cannot connect to the address");
+  }
+
+  if (send(sock, libpath.c_str(), libpath.size(), 0) < 0) {
+    close(sock);
+    throw std::logic_error("Cannot send a lib path to a server");
+  }
+
+  if (recv(sock, &result, sizeof(result), 0) < 0) {
+    close(sock);
+    throw std::logic_error("Cannot receive response from the server");
+  }
+
+  close(sock);
+
+  return result;
 }
 
 std::vector<response> runAllTests(std::vector<std::string> tests, std::string libpath)
 {
+  std::vector<response> testResults;
+  for (const auto& sock : tests) {
+    try {
+      testResults.push_back(runOneTest(sock, libpath));
+    }
+    catch(const std::logic_error& err) {
+      std::cerr << err.what() << std::endl;
+    }
+  }
+
+  return testResults;
 }
 
-std:vector<std::string> getTests(char *workerFile)
+std::vector<std::string> getTests(const std::string& workerFile)
 {
+  std::vector<std::string> sockets;
+  std::string line;
+  std::ifstream socketsFile(workerFile.c_str());
+
+  if (socketsFile.is_open()) {
+    while (std::getline(socketsFile, line)) {
+      sockets.push_back(line);
+    }
+  }
+
+  return sockets;
 }
 
 int main(int argc, char **argv)
 {
+  if (argc < 2) {
+    std::cerr << "Libpath argument is missing" << std::endl;
+    return 1;
+  }
+
   auto tests = getTests("worker-data.txt");
-  auto responses = runAllTests(tests);
+
+  auto libname = argv[1];
+  auto responses = runAllTests(tests, libname);
+
+  for (const auto& response : responses) {
+    std::cout << "===========================================" << std::endl;
+    std::cout << response.msg << std::endl;
+    std::cout << response.dist << std::endl;
+    std::cout << response.success << std::endl;
+  }
 }
