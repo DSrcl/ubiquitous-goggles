6c0c46753abbdba818d0d3c3e8a99680706058a5
Yishen Chen <cyt046@gmail.com>
2016-04-16 17:00:44 -0500

    working

---
+++ transform.cpp
@@ -0,0 +1,79 @@
+#include "transform.h"
+#include <cstdlib>
+
+using namespace llvm;
+
+void Transformation::Undo()
+{
+  assert(!Undone);
+  
+
+  switch (PrevTransformation) {
+  case MUT_OPCODE:
+  case MUT_OPERAND:
+  case REPLACE: {
+    auto *MBB = New->getParent();
+    MBB->insert(InstrIterator(New), Old);
+    New->eraseFromParent();
+    break; 
+  }
+
+  case INSERT: {
+    New->eraseFromParent();
+    break;
+  }
+
+  case SWAP: {
+    doSwap(Swapped1, Swapped2);
+    break;
+  }
+
+  case MOVE:
+    New->removeFromParent();
+
+    // undo MOVE and DELET by inserting the instruction back
+    // to its previous position
+  case DELETE:
+    auto *MBB = New->getParent();
+    if (NextLoc == MBB->end()) {
+      MBB->push_back(New);
+    } else {
+      MBB->insert(NextLoc, New);
+    }
+    break;
+  }
+
+  Undone = true;
+}
+
+Transformation::InstrIterator Transformation::select(Transformation::InstrIterator Except)
+{
+  InstrIterator Selected;
+  auto &MBB = *MF->begin();
+
+  assert(NumInstrs > 1 || MBB.begin() != Except);
+
+  do {
+    unsigned Idx = (unsigned)(rand() % (int)(NumInstrs + 1));
+    Selected = MBB.begin() + Idx;
+  } while (Selected == Except);
+
+  return Selected;
+}
+
+void Transformation::doSwap(InstrIterator A, InstrIterator B)
+{
+  if (A == B) return;
+
+  
+}
+
+void Transformation::swap()
+{
+  if (NumInstrs == 1) return;
+
+  auto &MBB = *MF->begin();
+  auto A = select(MBB->end()),
+       B = select(A);
+  doSwap(A, B);
+}
---
+++ transform.h
@@ -0,0 +1,62 @@
+#ifndef _TRANSFORM_H_
+#define _TRANSFORM_H_
+
+#include <llvm/CodeGen/MachineFunction.h>
+#include <llvm/CodeGen/MachineBasicBlock.h>
+#include <llvm/CodeGen/MachineInstr.h>
+
+class Transformation {
+  llvm::MachineFunction *MF;
+
+  unsigned NumInstrs;
+
+  enum Kind {
+    MUT_OPCODE,
+    MUT_OPERAND,
+    SWAP, 
+    REPLACE,
+    MOVE,
+    INSERT,
+    DELETE
+  } PrevTransformation;
+
+  // purely for debugging purpose because you can't undo more than once
+  bool Undone;
+
+  typedef llvm::MachineBasicBlock::instr_iterator InstrIterator;
+
+  // ------- states for undo ------------
+  // identifying previous position of an instruction,
+  // used for move and delete
+  InstrIterator NextLoc;
+
+  // for mutate* and replace, and partially for insert
+  llvm::MachineInstr *Old, *New;
+
+  // for swap
+  InstrIterator Swapped1, Swapped2;
+
+  // select a random instruction
+  InstrIterator select(InstrIterator Except);
+  // -----------------------
+
+  void doSwap(InstrIterator A, InstrIterator B);
+
+public:
+  Transformation(llvm::MachineFunction *TheMF) : MF(TheMF) {
+    assert(MF->size() == 1 && "no jumps for now");
+    NumInstrs = MF->begin()->size();
+  }
+
+  void Undo();
+
+  void MutateOpcode();
+  void MutateOperand();
+  void Swap();
+  void Replace();
+  void Move();
+  void Insert();
+  void Delete();
+};
+
+#endif
